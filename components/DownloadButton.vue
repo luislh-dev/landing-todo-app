25548690d99917410dfa6c0289f97e92e1a0196e
luislopez-developer <luislopezhuari27@gmail.com>
2025-02-20 14:50:51 -0500

    Feat: agregar animación de confeti al botón de descarga

--- components/DownloadButton.vue
+++ components/DownloadButton.vue
@@ -1,16 +1,29 @@
 <template>
-  <button @click="downloadApk"
-    :class="['flex gap-1.5 items-center font-semibold bg-violet-500 shadow-xs justify-center text-white p-2 rounded-lg hover:bg-violet-600', className]">
-    <p>Descargar</p>
-    <Icon name="heroicons:arrow-down-tray-16-solid" size="16" />
+  <button @click="handleDownload"
+    :class="['flex gap-1.5 items-center font-semibold bg-fuchsia-600 shadow-xs justify-center text-white p-2 rounded-3xl hover:bg-fuchsia-600/80', className]">
+    Descargar
   </button>
 </template>
 
 <script setup>
+import JSConfetti from 'js-confetti';
+
 defineProps({
   className: {
     type: String,
     default: ''
   }
 });
+
+let jsConfetti;
+
+onMounted(() => {
+  jsConfetti = new JSConfetti();
+});
+
+const handleDownload = () => {
+  jsConfetti.addConfetti();
+  downloadApk();
+};
+
 </script>
