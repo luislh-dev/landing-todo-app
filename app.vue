caa9a5dea5671894b4d03313f462edcefe248f9d
luislopez-developer <luislopezhuari27@gmail.com>
2025-02-22 17:18:14 -0500

    Feat: actualizar configuración de Prettier, añadir soporte para TypeScript y agregar sitemap en robots.txt

--- app.vue
+++ app.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: 'es'
+  },
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Todo App` : 'Todo App';
+  },
+  meta: [
+    { name: 'format-detection', content: 'telephone=no' },
+    { name: 'theme-color', content: '#ffffff' },
+    { name: 'description', content: 'Todo App es una aplicación de gestión de tareas que te permite organizar tu día a día de forma eficiente. Con Todo App, podrás añadir tareas, categorizarlas, establecer recordatorios y sincronizar tus datos en la nube.' }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+  ]
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxtjs/sitemap",
+  ],
+  site: {
+	hostname: "https://todoapp.luislh.dev"
+  },
   image: {
     format: ["webp", "avif"],
   },
