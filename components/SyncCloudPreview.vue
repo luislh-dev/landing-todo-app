19cf1bdf8058dc6a78814d20c7b57c3b2ef09833
luislopez-developer <luislopezhuari27@gmail.com>
2025-02-21 22:21:23 -0500

    Feat: actualizar diseño de vista previa de sincronización en la nube con nuevo encabezado y toggle decorativo

--- components/SyncCloudPreview.vue
+++ components/SyncCloudPreview.vue
@@ -2,12 +2,23 @@
   <div>
     <div
       class="bg-white p-6 rounded-lg shadow-lg w-[360px] lg:w-[400px] 2xl:w-[580px] space-y-2 text-sm lg:text-base 2xl:text-2xl">
-      <div class="flex items-center gap-2">
-        <Icon name="heroicons:arrow-path-16-solid" class="text-fuchsia-600" />
-        <h3 class="font-bold text-gray-800">Sincronizar tareas en la nube</h3>
+      <div class="flex items-center justify-between pb-4">
+        <div class="flex items-center gap-2">
+          <Icon name="heroicons:arrow-path-16-solid" class="text-fuchsia-600" />
+          <h3 class="font-bold text-gray-800">Sincronización automática</h3>
+        </div>
+        <!-- toggle decorativo-->
+        <div className="relative inline-flex items-center">
+          <div className="w-14 h-8 bg-fuchsia-600 rounded-full">
+            <div
+              className="absolute left-8 top-2 w-4 h-4 bg-white rounded-full transition-all duration-300 ease-in-out">
+            </div>
+          </div>
+        </div>
       </div>
-      <p class="text-gray-600">Guarda tus tareas y categorías en tu Google Drive como copia de seguridad.</p>
-      <p class="text-fuchsia-700 text-xs lg:text-sm 2xl:text-xl">Sincronizar</p>
+      <div class="h-[15px] bg-gray-200 rounded-lg" />
+      <div class="h-[15px] bg-gray-200 rounded-lg" />
+      <div class="h-[15px] w-[300px] bg-gray-200 rounded-lg" />
     </div>
   </div>
 </template>
